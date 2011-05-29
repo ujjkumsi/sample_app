d9a48eb605058f785563e45eb7b38a0c807b29a0
ujjwal <ujjwal014658@gmail.com>
2011-05-29 08:41:26 -0700

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+get "pages/home"
+get "pages/contact"
+get "pages/about"  
+get "users/new"
+
   match '/contact', :to => 'pages#contact'
 match '/about', :to => 'pages#about'
 match '/help', :to => 'pages#help'
